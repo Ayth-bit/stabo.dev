b2b972cc22fc300275703acf351b93b320d7d189
ayth-bit <slo.overwatch@gmail.com>
2025-07-12 21:16:42 +0900

    デプロイエラーの修正

--- public/project/src/main.tsx
+++ public/project/src/main.tsx
@@ -1,6 +1,8 @@
+// public/project/src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+// ★★★ './App.tsx' から '.tsx' を削除 ★★★
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
